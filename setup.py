1184ddc400935027bd24a2adcd8488d41e3be029
Raushan0502 <rk7250846@gmail.com>
2025-04-03 02:23:43 +0530

    Template ready, installed required package successfully and mongodb atlas setup also done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",     # Replace with your package name
+    version="0.0.1",  # Replace with your package version   
+    author="Raushan Kumar",  # Replace with your name
+    author_email= "rk7250846@gmail.com",
+    packages= find_packages(),  # Automatically find packages in the current directory
+)
