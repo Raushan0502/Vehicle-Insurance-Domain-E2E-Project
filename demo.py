b498185d3738f696c37ea225c2d22ae500d09e84
Raushan0502 <rk7250846@gmail.com>
2025-06-28 17:11:25 +0530

    Data ingestion part completed!!

--- demo.py
+++ demo.py
@@ -15,14 +15,15 @@ from src.exception import MyException
 import sys
 
 try:
-    a = 1+'Z'
+    #a = 1+'Z'
+    a = 1 +3
 except Exception as e:
     logging.info(e)
     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipeline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
