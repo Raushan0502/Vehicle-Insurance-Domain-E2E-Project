383c9505b9f93f36ee2789cace5ab9e430ae8c9d
Raushan0502 <rk7250846@gmail.com>
2025-07-06 01:13:56 +0530

    ready for deployment, model push to s3 bucket

---
+++ src/configuration/aws_connection.py
@@ -0,0 +1,49 @@
+import boto3
+import os
+from src.constants import AWS_SECRET_ACCESS_KEY_ENV_KEY, AWS_ACCESS_KEY_ID_ENV_KEY, REGION_NAME
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
+
+class S3Client:
+
+    s3_client=None
+    s3_resource = None
+    def __init__(self, region_name=REGION_NAME):
+        """ 
+        This Class gets aws credentials from env_variable and creates an connection with s3 bucket 
+        and raise exception when environment variable is not set
+        """
+
+        if S3Client.s3_resource==None or S3Client.s3_client==None:
+            __access_key_id = os.getenv("AWS_ACCESS_KEY_ID_ENV_KEY")
+            __secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY_ENV_KEY")
+            print(f"Access Key ID: {__access_key_id}")
+            print(f"Secret Access Key: {__secret_access_key}")
+
+            if __access_key_id is None:
+                raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not not set.")
+            if __secret_access_key is None:
+                raise Exception(f"Environment variable: {AWS_SECRET_ACCESS_KEY_ENV_KEY} is not set.")
+        
+            S3Client.s3_resource = boto3.resource('s3',
+                                            aws_access_key_id=__access_key_id,
+                                            aws_secret_access_key=__secret_access_key,
+                                            region_name=region_name
+                                            )
+            S3Client.s3_client = boto3.client('s3',
+                                        aws_access_key_id=__access_key_id,
+                                        aws_secret_access_key=__secret_access_key,
+                                        region_name=region_name
+                                        )
+        self.s3_resource = S3Client.s3_resource
+        self.s3_client = S3Client.s3_client
+
+s3 = S3Client()
+def get_s3_client():
+    """
+    Returns the S3 client instance.
+    """
+    return s3.s3_client, s3.s3_resource
+
